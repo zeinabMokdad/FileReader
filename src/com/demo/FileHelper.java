e02dee7e06ad9fc0e328e08e853d4d4053016cfd
zeinabMokdad <zeinab.m0okdad@gmail.com>
2020-05-09 11:23:23 +0300

    file reader

--- src/com/demo/FileHelper.java
+++ src/com/demo/FileHelper.java
@@ -4,6 +4,8 @@ import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.Format;
+import java.util.Formatter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
@@ -40,15 +42,18 @@ public class FileHelper {
     private void processLine(String line, Map<String, Integer> wordsCount) {
         String[] words = line.split(" ");
         for (String word : words) {
-
-            if(word.isEmpty() || word.isBlank())
+            if (word.isEmpty() || word.isBlank())
                 continue;
-
             String lowerCaseWord = word.toLowerCase();
-            wordsCount.merge(lowerCaseWord, 1, Integer::sum);
+            Integer wordCount = wordsCount.get(lowerCaseWord);
+            if (wordCount == null)
+                wordsCount.put(lowerCaseWord, 1);
+            else
+                wordsCount.put(lowerCaseWord, wordCount + 1);
         }
     }
-    private void Write(Map<String, Integer> wordsCount, File outputFile) throws IOException {
+
+    public void Write(Map<String, Integer> wordsCount, File outputFile) throws IOException {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outputFile))) {
             for (Map.Entry<String, Integer> entry : wordsCount.entrySet()) {
                 bufferedWriter.write(entry.getKey() + ":" + entry.getValue());
@@ -59,4 +64,26 @@ public class FileHelper {
             System.out.println(exc.getMessage());
         }
     }
+
+    public void Write(InputStream inputStream, File outputFile) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+            int read;
+            byte[] bytes = new byte[1024];
+            while ((read = inputStream.read(bytes)) != -1)
+                fileOutputStream.write(bytes, 0, read);
+        }
+    }
+
+    private void doWrite(Map<String, Integer> wordsCount, File outputFile) throws IOException {
+
+        BufferedWriter bw = Files.newBufferedWriter(Paths.get("output.txt"));
+
+        for (Map.Entry<String, Integer> entry : wordsCount.entrySet()) {
+            try (Formatter f = new Formatter(bw)) {
+                f.format("%s : %d", entry.getKey(), entry.getValue());
+            }
+        }
+    }
 }
+
+
